ab130acae4337b319cd1eab41203129ff25b4950
lorenzo.scaduto2001@gmail.com <68859207+Scaddy00@users.noreply.github.com>
2025-06-12 14:18:13 +0200

    Aggiunto evento Byebye
    Evento che logga quando un utente esce dal server

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from logger.logger import Logger
 bot: discord.Client
 log: Logger
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ERROR_CHANNEL_ID: int = int(str(getenv('ERROR_CHANNEL_ID')))
+BOT_COMMUNICATION_CHANNEL_ID: int = int(str(getenv('BOT_COMMUNICATION_CHANNEL_ID')))
+ADMIN_CHANNEL_ID: int = int(str(getenv('ADMIN_CHANNEL_ID')))
 
 # ============================= BOT SETUP =============================
 intents = discord.Intents.all()
@@ -20,24 +21,43 @@ bot = commands.Bot(command_prefix=str(getenv('COMMAND_PREFIX')), intents=intents
 
 
 # ============================= ON_MEMBER_JOIN (Welcome) =============================
-@bot.event # type: ignore
+@bot.event
 async def on_member_join(member: discord.Member) -> None:
-    # Load error channel
-    error_channel = bot.get_channel(ERROR_CHANNEL_ID)
+    # Load bot communication channel
+    communication_channel = bot.get_channel(BOT_COMMUNICATION_CHANNEL_ID)
     # Get guild
     guild: discord.Guild = member.guild
     
     try:
-        welcome_channel: discord.TextChannel = guild.system_channel # type: ignore
+        welcome_channel: discord.TextChannel = guild.system_channel
+        # TODO Update the message with the Embed
         message: str = f'Benvenuto {member.mention}!'
         await welcome_channel.send(message)
         # INFO LOG
-        await log.event(f'Nuovo utente aggiunto, {member.name}({member.id})', 'welcome')
+        await log.event(f'Nuovo utente aggiunto, {member.name} ({member.id})', 'welcome')
     except Exception as e:
         # EXCEPTION
-        error_message: str = log.error_message(command = 'EVENT - WELCOME', message = f'Errore durante l\'invio del messaggio di benvenuto \n{e}')
-        await log.error(error_message, 'EVENT - WELCOME')
-        await error_channel.send(error_message) # type: ignore
+        error_message: str = f'Errore durante l\'invio del messaggio di benvenuto. \nUtente: {member.name} ({member.id}) \n{e}'
+        await log.error(error_message, 'EVENT - MEMBER WELCOME')
+        await communication_channel.send(log.error_message(command = 'EVENT - WELCOME', message = error_message))
+
+# ============================= ON_MEMBER_REMOVE (ByeBye) =============================
+@bot.event
+async def on_raw_member_remove(payload: discord.RawMemberRemoveEvent) -> None:
+    # Load bot communication channel
+    communication_channel = bot.get_channel(BOT_COMMUNICATION_CHANNEL_ID)
+    # Get the user
+    user: discord.User = payload.user
+    
+    try:
+        await communication_channel.send(f'COMUNICAZIONE -> L\'utente {user.mention} ha lasciato il server')
+        # INFO LOG
+        await log.event(f'Utente uscito dal server, {user.name} ({user.id})', 'remove')
+    except Exception as e:
+        # EXCEPTION
+        error_message: str = f'Errore durante l\'invio del messaggio di ByeBye. \nUtente: {user.name} ({user.id})\n{e}'
+        await log.error(error_message, 'EVENT - MEMBER REMOVE')
+        await communication_channel.send(log.error_message(command = 'EVENT - MEMBER REMOVE', message = error_message))
 
 # ============================= MAIN =============================
 def main():
--- utility/printing.py
+++ utility/printing.py
@@ -1,8 +1,54 @@
+
 # ----------------------------- Standard library -----------------------------
 from datetime import datetime
 from os import getenv
+from discord import Embed
 
 # ============================= Datetime format =============================
 def format_datetime_now() -> str:
     str_format: str = str(getenv('DATETIME_FORMAT'))
     return datetime.now().strftime(str_format)
+
+# ============================= Embed creator =============================
+def create_embed(title: str, description: str, color: int, url: str | None = None, fields: list = [], footer: dict = {}, thumbnail: str = '', image: str = '') -> Embed:
+    """Function for creating Discord Embeds
+
+    Args:
+        title (str): Title of the embed
+        description (str): Description of the embed
+        color (int): Color of the embed. Color format ...
+        url (str | None, optional): Insert a url in the embed. Defaults to None.
+        fields (list, optional): A list of fields. Defaults to [].
+        footer (dict, optional): Footer of the embed. Defaults to {}.
+        thumbnail (str, optional): Thumbnail url of the embed. Defaults to ''.
+        image (str, optional): Image url of the embed. Defaults to ''.
+
+    Returns:
+        Embed: _description_
+    """
+    
+    # Create the embed with the standard information
+    embed: Embed = Embed(title=title,
+                         type='rich',
+                         description=description,
+                         color=color,
+                         url=url,
+                         )
+    # Add fields to the embed
+    if len(fields) > 0:
+        for field in fields:
+            embed.add_field(name=field['name'],
+                            value=field['value'],
+                            inline=field['inline'])
+    # Add footer to the embed
+    if footer:
+        embed.set_footer(text=footer['text'],
+                         icon_url=footer['icon_url'])
+    # Add thumbnail to the embed
+    if thumbnail != '':
+        embed.set_thumbnail(url=thumbnail)
+    # Add image to the embed
+    if image != '':
+        embed.set_image(url=image)
+
+    return embed
