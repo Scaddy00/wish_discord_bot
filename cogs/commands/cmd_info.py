9e2d7d734e51214bc359ee1f4e19f279ef8de494
lorenzo.scaduto2001@gmail.com <68859207+Scaddy00@users.noreply.github.com>
2025-07-20 22:14:42 +0200

    Update CmdInfo commands to enhance functionality and logging
    - Renamed the 'dreamer' command to 'dreamer-unico' for clarity and updated related logging messages.
    - Added a new 'dreamer-sub' command to provide information on subscription levels, including enhanced logging and error handling.
    - Adjusted embed loading logic to accommodate the new command structure.

--- cogs/commands/cmd_info.py
+++ cogs/commands/cmd_info.py
@@ -6,7 +6,7 @@ from discord import app_commands
 from os import getenv
 # ----------------------------- Custom Libraries -----------------------------
 from logger import Logger
-from utils.printing import create_embed_from_dict, load_single_embed_text
+from utils.printing import create_embed_from_dict, load_single_embed_text, load_embed_text
 from config_manager import ConfigManager
 
 class CmdInfo(commands.GroupCog, name="info"):
@@ -16,7 +16,7 @@ class CmdInfo(commands.GroupCog, name="info"):
         self.log = log
         self.config = config
     
-    @app_commands.command(name="dreamer", description="Invia un embed con le info per avere un Dreamer personalizzato")
+    @app_commands.command(name="dreamer-unico", description="Invia un embed con le info per avere un Dreamer unico")
     async def dreamer(self, interaction: discord.Interaction, channel: discord.TextChannel) -> None:
         # Get the guild from interaction
         guild: discord.Guild = interaction.guild
@@ -24,19 +24,19 @@ class CmdInfo(commands.GroupCog, name="info"):
         communication_channel = guild.get_channel(self.config.communication_channel)
         
         # INFO Log the start of the creation of the message
-        await self.log.command('Creazione di un nuovo messaggio', 'info', 'DREAMER')
+        await self.log.command('Creazione di un nuovo messaggio', 'info', 'DREAMER UNICO')
         
         try:
             # Load embed message content
-            message_content: dict = await load_single_embed_text(guild, 'info-dreamer', self.config)
+            message_content: dict = await load_single_embed_text(guild, 'info-dreamer-unico', self.config)
             # Create the embed message
-            message: dict = create_embed_from_dict(message_content)
+            message: discord.Embed = create_embed_from_dict(message_content)
             
             # Send the message in the selected channel
             await channel.send(embed=message)
             
             # INFO Log that the embed was sent
-            await self.log.command('Messaggio inviato', 'info', 'DREAMER')
+            await self.log.command('Messaggio inviato', 'info', 'DREAMER UNICO')
             
             # Send the response to the initial message
             await interaction.response.send_message('Il messaggio è stato inviato correttamente', ephemeral=True)
@@ -44,5 +44,37 @@ class CmdInfo(commands.GroupCog, name="info"):
         except Exception as e:
             # EXCEPTION
             error_message: str = f'Errore durante la creazione e l\'invio di un nuovo messaggio.\n{e}'
-            await self.log.error(error_message, 'COMMAND - INFO - DREAMER')
-            await communication_channel.send(self.log.error_message(command='COMMAND - INFO - DREAMER', message=error_message))
+            await self.log.error(error_message, 'COMMAND - INFO - DREAMER UNICO')
+            await communication_channel.send(self.log.error_message(command='COMMAND - INFO - DREAMER UNICO', message=error_message))
+    
+    @app_commands.command(name="dreamer-sub", description="Invia un embed con le info dei vari livelli di abbonamento")
+    async def dreamer_sub(self, interaction: discord.Interaction, channel: discord.TextChannel) -> None:
+        # Get the guild from interaction
+        guild: discord.Guild = interaction.guild
+        # Load communication channel
+        communication_channel = guild.get_channel(self.config.communication_channel)
+        
+        # INFO Log the start of the creation of the message
+        await self.log.command('Creazione di un nuovo messaggio', 'info', 'DREAMER SUB')
+        
+        try:
+            # Load embed message content
+            message_content: dict = await load_embed_text(guild, 'info-dreamer-sub', self.config)
+            # Create the embed message
+            message: list[discord.Embed] = [create_embed_from_dict(item) for item in message_content]
+            
+            # Send the message in the selected channel
+            await channel.send(embeds=message)
+            
+            # INFO Log that the embed was sent
+            await self.log.command('Messaggio inviato', 'info', 'DREAMER SUB')
+            
+            # Send the response to the initial message
+            await interaction.response.send_message('Il messaggio è stato inviato correttamente', ephemeral=True)
+        
+        except Exception as e:
+            # EXCEPTION
+            error_message: str = f'Errore durante la creazione e l\'invio di un nuovo messaggio.\n{e}'
+            await self.log.error(error_message, 'COMMAND - INFO - DREAMER SUB')
+            await communication_channel.send(self.log.error_message(command='COMMAND - INFO - DREAMER SUB', message=error_message))
+    
